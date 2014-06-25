44e51fe10c7dcaff5a8b0d41e2132370b0ea5039
Kevin Murray <spam@kdmurray.id.au>
2014-06-25 14:57:10 +1000

    add demux script

---
+++ demux.sh
@@ -0,0 +1,5 @@
+if [ -z "$(md5sum reads/CAGRF5704/BJP-277-A_D10GVACXX_NoIndex_L002_R1.fastq.gz | grep e3a3769a21df23593f1d959cd9783dd5)" ]
+then
+	echo "File md5sum isn't e3a3769a21df23593f1d959cd9783dd5"
+fi
+pigz -dcp3 reads/CAGRF5704/BJP-277-A_D10GVACXX_NoIndex_L002_R1.fastq.gz  |python demux.py /dev/stdin out2/%s.fq
