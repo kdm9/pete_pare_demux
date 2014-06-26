541a6fb70faaae4937dbe7e958325c2ddddb14dd
Kevin Murray <spam@kdmurray.id.au>
2014-06-26 14:29:36 +1000

    [demux.sh] add sorting of leftover to script

--- demux.sh
+++ demux.sh
@@ -2,4 +2,6 @@ if [ -z "$(md5sum reads/CAGRF5704/BJP-277-A_D10GVACXX_NoIndex_L002_R1.fastq.gz |
 then
 	echo "File md5sum isn't e3a3769a21df23593f1d959cd9783dd5"
 fi
+mkdir out2
 pigz -dcp3 reads/CAGRF5704/BJP-277-A_D10GVACXX_NoIndex_L002_R1.fastq.gz  |python demux.py /dev/stdin out2/%s.fq
+time python sort_leftovers.py out2/NOBCD.fq >rebarcoded.fq
